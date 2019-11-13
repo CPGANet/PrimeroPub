5c05e1c20c6954485ee2e1a22d9b6528152b93e5
CPGANet <soportecpganet@gmail.com>
2019-11-13 16:02:58 -0300

    Commit desde terminal

---
+++ Primero/src/UnaClase/UnaClase.java
@@ -0,0 +1,26 @@
+package UnaClase;
+
+public class UnaClase {
+    public static void main(String[] args) {
+        String str = "Primera Clase Escrita con Formato CamelCase";
+        String str1 = "", str2 = "";
+        System.out.println("");
+        System.out.println(str);
+        System.out.println();
+        for (int i = 0; i < str.length(); i++) {
+            int ascii = str.charAt(i);
+            if (ascii <= 90) {
+                str1 += str.charAt(i);
+                str2 += " ";
+            } else {
+                str2 += str.charAt(i);
+                str1 += " ";
+            }
+        }
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
+
+
+
